2b77617d32adb47f4d98d75d374b148fa84f626d
Christoforos Santorinaios <Christoforos Santorinaios>
2016-02-27 18:07:21 +0000

    code for step 1, step 2, and calculation of J

--- kClustering.m
+++ kClustering.m
@@ -37,17 +37,43 @@ colormap jet; % set nice colormap with distinct colors
 scatter3(red(:),green(:),blue(:),[],idx(:),'.') %scatter all points in 3d, color them according to their cluster. Uses the current colormap
 
 
-means=ceil(rand(K,3)*255); %the k-means points. We choose them randomly.
-squaredNorms=zeros(K,1);
-for n=1:N
-    for i=1:K
-        squaredNorms(i)=norm(d(n)-means(i))^2;
+
+%Step 1
+
+means=ceil(rand(K,3)*255); %the k-means points initialisations. We choose them randomly.
+squaredNorms=zeros(K,1);  %initialise a vector of squared norm values to zeros
+
+
+for n=1:N                %for all datapoints
+    for i=1:K           %for all clusters
+        squaredNorms(i)=norm(d(n)-means(i))^2;    %calc distances from all datapoints to cluster's mean
         
     end
-    [minimum,index]=min(squaredNorms);
-    r(n,index)=1;
+    [minimum,index]=min(squaredNorms); 
+    r(n,index)=1; 
+    
+end
+% r now holds 1 hot notation of which pixel goes to which cluster 
+
+%Step 2
+
+for i=1:K
+    
+    numerator=0;
+    denominator=0;
+    for n=1:N
+        numerator=numerator+r(n,i)*d(n);
+        denominator=denominator+r(n,i);
+    end
+    means(i)=numerator/denominator;
     
 end
 
-
+%calc J 
+J=0;
+for n=1:N
+    for i=1:K
+        J=J+r(n,i)*norm(d(n)-means(i))^2;
+    end
+end
 
