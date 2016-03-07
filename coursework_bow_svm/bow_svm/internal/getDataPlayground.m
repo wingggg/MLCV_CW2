370b938adfafcc0209105b1eeb5df2853db259a2
Christoforos Santorinaios <Christoforos Santorinaios>
2016-03-07 20:29:54 +0000

    attempts at Q3. Work in progress

---
+++ coursework_bow_svm/bow_svm/internal/getDataPlayground.m
@@ -0,0 +1,162 @@
+MODE='Caltech';
+% Generate training and testing data
+
+% Data Options:
+%   1. Toy_Gaussian
+%   2. Toy_Spiral
+%   3. Toy_Circle
+%   4. Caltech 101
+
+showImg = 0; % Show training & testing images and their image feature vector (histogram representation)
+
+PHOW_Sizes = [4 8 10]; % Multi-resolution, these values determine the scale of each layer.
+PHOW_Step = 8; % The lower the denser. Select from {2,4,8,16}
+
+switch MODE
+        
+    case 'Caltech' % Caltech dataset
+        close all;
+        imgSel = [15 15]; % randomly select 15 images each class without replacement. (For both training & testing)
+        folderName = '../Caltech_101/101_ObjectCategories';
+        classList = dir(folderName);
+        classList = {classList(3:end).name}; % 10 classes
+        
+        disp('Loading training images...')
+        % Load Images -> Description (Dense SIFT)
+        cnt = 1;
+        if showImg
+            figure('Units','normalized','Position',[.05 .1 .4 .9]);
+            suptitle('Training image samples');
+        end
+        for c = 1:length(classList)
+            subFolderName = fullfile(folderName,classList{c});
+            imgList = dir(fullfile(subFolderName,'*.jpg'));
+            imgIdx{c} = randperm(length(imgList));
+            imgIdx_tr = imgIdx{c}(1:imgSel(1));
+            imgIdx_te = imgIdx{c}(imgSel(1)+1:sum(imgSel));
+            
+            for i = 1:length(imgIdx_tr)
+                I = imread(fullfile(subFolderName,imgList(imgIdx_tr(i)).name));
+                
+                % Visualise
+                if i < 6 && showImg
+                    subaxis(length(classList),5,cnt,'SpacingVert',0,'MR',0);
+                    imshow(I);
+                    cnt = cnt+1;
+                    drawnow;
+                end
+                
+                if size(I,3) == 3
+                    I = rgb2gray(I); % PHOW work on gray scale image
+                end
+                
+                % For details of image description, see http://www.vlfeat.org/matlab/vl_phow.html
+                [~, desc_tr{c,i}] = vl_phow(single(I),'Sizes',PHOW_Sizes,'Step',PHOW_Step); %  extracts PHOW features (multi-scaled Dense SIFT)
+            end
+        end
+        
+        disp('Building visual codebook...')
+        % Build visual vocabulary (codebook) for 'Bag-of-Words method'
+        desc_sel = single(vl_colsubset(cat(2,desc_tr{:}), 10e4)); % Randomly select 100k SIFT descriptors for clustering
+        
+  
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % K-means clustering
+        % write your own codes here
+        K=5;
+        
+        [idx,Cmeans]=kmeans(desc_sel',K);
+        %Now Cmeans is the K codewords.
+        %idx describes how each of the 100k descriptors got classified into
+        %the K codewords
+        
+        
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+       
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        disp('Encoding Images...')
+        % Vector Quantisation
+        
+        %Code below creates the histogram of a test image. IDX are its
+        %descriptor classifications
+%         hist=zeros(K,1);
+%         for i=1:length(IDX)
+%             for j=1:K
+%                 if IDX(i)==j
+%                     hist(j)=hist(j)+1;
+%                 end
+%             end
+%         end
+        
+        % write your own codes here
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        
+        
+        % data_train: [num_data x (dim+1)] Training vectors with class labels
+        % data_train(:,end): class labels
+        data_train = [];
+        
+       
+        % Clear unused varibles to save memory
+        %clearvars  desc_sel
+end
+
+switch MODE
+    case 'Caltech'
+        if showImg
+        figure('Units','normalized','Position',[.05 .1 .4 .9]);
+        suptitle('Testing image samples');
+        end
+        disp('Processing testing images...');
+        cnt = 1;
+        % Load Images -> Description (Dense SIFT)
+        for c = 1:length(classList)
+            subFolderName = fullfile(folderName,classList{c});
+            imgList = dir(fullfile(subFolderName,'*.jpg'));
+            imgIdx_te = imgIdx{c}(imgSel(1)+1:sum(imgSel));
+            
+            for i = 1:length(imgIdx_te)
+                I = imread(fullfile(subFolderName,imgList(imgIdx_te(i)).name));
+                
+                % Visualise
+                if i < 6 & showImg
+                    subaxis(length(classList),5,cnt,'SpacingVert',0,'MR',0);
+                    imshow(I);
+                    cnt = cnt+1;
+                    drawnow;
+                end
+                
+                if size(I,3) == 3
+                    I = rgb2gray(I);
+                end
+                [~, desc_te{c,i}] = vl_phow(single(I),'Sizes',PHOW_Sizes,'Step',PHOW_Step);
+            
+            end
+        end
+    
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Vector Quantisation
+        % write your own codes here
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+        
+        
+        % data_query: [num_data x (dim+1)] Testing vectors with class labels
+        % data_query(:,end): class labels
+        data_query = [];
+        
+        
+        
+    otherwise % Dense point for 2D toy data
+        xrange = [-1.5 1.5];
+        yrange = [-1.5 1.5];
+        inc = 0.02;
+        [x, y] = meshgrid(xrange(1):inc:xrange(2), yrange(1):inc:yrange(2));
+        data_query = [x(:) y(:) zeros(length(x)^2,1)];
+end
+
+
