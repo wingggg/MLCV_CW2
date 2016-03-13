f136e9603ed986af8320bb7b19ca1a0acdfec840
cs3712 <cs3712@ic.ac.uk>
2016-03-13 19:18:58 +0000

    added some comments to Q3.m

--- coursework_bow_svm/bow_svm/internal/Q3.m
+++ coursework_bow_svm/bow_svm/internal/Q3.m
@@ -2,27 +2,25 @@
 clear all;
 %addpath('../external/libsvm-3.18');
 [data_train, data_query]=getData('Caltech');
-labels=data_train(:,end);
+labels=data_train(:,end); %extract labels from training data
 
 %we can only use svmtrain with two classes at a time. 
 % We will first try one vs many.
 
-%A) One vs Many
-%training svms 
-svmStructs=[];
+%One vs Many
+%------------------------
+%Initialise structs
+svmStructs=[];    
 svmPredictions=[];
 prob=[];
+
+%we use the temp variables to
 data_trainTemp=data_train;
 data_queryTemp=data_query;
-
-
 %===========================
-
-
+%Code below is to do consequent one vs rest svm trainings
 for i=1:length(unique(labels)) %for each class
-    
     for j=1:size(data_train,1)  %for each data sample
-        
         if data_train(j,end)==i     %make a temp matrix with class i being 1 and all other classes being 0 for train data
             data_trainTemp(j,end)=1;   
         else
@@ -36,19 +34,18 @@ for i=1:length(unique(labels)) %for each class
             data_queryTemp(j,end)=0;
         end
     end
-    
-    %svm goes here
+   
     
     %bestGamma=0.2;
     %bestC=10000;
     
-    [bestC, bestGamma]=gridSearch(data_trainTemp,-5,15,-15,3);
-    cmd=[' -c ',num2str(bestC), ' -g ', num2str(bestGamma), ' -b 1 -t 2 ']; %build the arguments tsring for svm train
+    [bestC, bestGamma]=gridSearch(data_trainTemp,-5,15,-15,3); %run a gridSearch with crossValidation to find the best C and gamma params
+    cmd=[' -c ',num2str(bestC), ' -g ', num2str(bestGamma), ' -b 1 -t 2 ']; %build the arguments string for svmtrain
     SVMStruct = svmtrain(data_trainTemp(:,end), data_trainTemp(:,1:end-1),cmd); %run svm for current pair of classes
-    svmStructs=[svmStructs SVMStruct]; %store in struct of svm results 
+    svmStructs=[svmStructs SVMStruct]; %store results in struct of svm results 
     
     %run prediction for query data
-    [predicted_label, accuracy, prob_estimates]=svmpredict(data_queryTemp(:,end), data_queryTemp(:,1:end-1), SVMStruct, '-b 1');
+    [predicted_label, accuracy, prob_estimates]=svmpredict(data_queryTemp(:,end), data_queryTemp(:,1:end-1), SVMStruct, '-b 1'); %predict the accuracy of the current svm
     
 
 %     [predicted_label, accuracy,prob_estimates]=svmpredict(data_queryTemp(:,end), [(1:150)', data_queryTemp(:,1:end-1)*data_trainTemp(:,1:end-1)'], SVMStruct);     SVMPredictStruct=[predicted_label, accuracy, prob_estimates];
@@ -68,13 +65,17 @@ indices=zeros(length(prob),1);  %init the indices matrix
 for i=1:length(prob)
     [~,indices(i)]=max(prob(i,:)); %find indices of maximum probs   %predicted class = indices(i)
 end
-multiClassLabels=indices;
+multiClassLabels=indices; %now multiClassLabels holds the labels our trained svm's gave to the data
 
+
+%We calculate the accuracy of our results below
 accuracy=zeros(length(predicted_label),1);
 for i=1:length(predicted_label)
-    accuracy(i)=(multiClassLabels(i)==data_query(i,3));
+    accuracy(i)=(multiClassLabels(i)==data_query(i,end)); %sum all the instances on which our prediction matches the test data actual label
 end
-percentageAccuracy=sum(accuracy)/length(predicted_label);
+percentageAccuracy=sum(accuracy)/length(predicted_label); %find the percentage of correct predictions
+
+%Confusion matrix code below  (?) 
 
 
 
